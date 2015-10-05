2441f8ce0da0540202f35aaae3cd26961dbced3f
Cserich Dávid <cserich.david@gmail.com>
2015-10-05 22:09:10 +0200

    Végleges

--- HF03_CserichD/HF03_CserichD.cpp
+++ HF03_CserichD/HF03_CserichD.cpp
@@ -1,9 +1,22 @@
 // Cserich David
-/*
- Készítsd el STL tárolók használata nélkül a körkörös puffer /FIFO tároló, körbeforduló írással, olvasással/ osztály sablont (CircularBuffer), amely tetszőleges típusú elemeket képes tárolni! A puffer mérete min. 1, max. 1000, amelyet a konstruktor paramétereként lehet megadni. Ha nincs megadva, vagy nincs az előbb megadott tartományban, akkor alapértelmezett mérete 10. A remove művelet kiveszi a legelső (legkorábban betett) elemet a pufferből, az insert művelet pedig a következő üres helyre teszi a paraméterként átadott elemet, amennyiben ez lehetséges (nincs felülírás!). Az isEmpty és isFull bool visszatérési típusú fv. információt ad a puffer állapotáról értelemszerűen (üres, megtelt). Üres pufferből történő kivételkor, illetve teli pufferbe történő íráskor keletkezzen kivételes esemény (std::exception osztályból leszámaztatott saját hiba osztály)! 
- */
 
 #include <iostream>
+
+using namespace std;
+
+class Kivetel: public exception {
+  int id;
+public:
+  Kivetel(int i) : id(i) {}
+  const char* what() const throw()
+  {
+    switch(id) {
+      default: return "Unexpected exception!\n";               break;
+      case 1:  return "Exception occured: Buffer is full!\n";  break;
+      case 2:  return "Exception occured: Buffer is empty!\n"; break;
+    }
+  }
+};
 
 template <class T>
 class CircularBuffer {
@@ -12,7 +25,7 @@ class CircularBuffer {
   int tail;
   int size;
 public:
-  CircularBuffer(int s = 10) {
+  explicit CircularBuffer(int s = 10) {
     if (( s > 0 ) && ( s <= 1000 )) {
       buffer = new T[s+1];
       size = s+1;
@@ -23,8 +36,11 @@ public:
     tail = 0;
     head = 0;
   }
+
   
-  bool isEmpty() {
+  int getSize() const { return size;}
+  
+  bool isEmpty() const {
     if (head == tail) {
       return true;
     } else {
@@ -32,7 +48,7 @@ public:
     }
   }
   
-  bool isFull() {
+  bool isFull() const {
     if (head == ( (tail + 1) % size )) {
       return true;
     } else {
@@ -40,18 +56,82 @@ public:
     }
   }
   
-  void insert(T element) {
+  void insert(const T element) {
     if (!isFull()) {
-      buffer[(tail++) % size] = element;
+      buffer[tail] = element;
+      tail = (tail + 1) % size;
+    } else {
+      throw Kivetel(1);
     }
   }
   
   T remove() {
     if (!isEmpty()) {
-      return buffer[(head++) % size];
+      T result = buffer[head];
+      head = (head + 1) % size;
+      return result;
     } else {
+      throw Kivetel(2);
       return NULL;
     }
   }
   
+  friend ostream& operator<<(ostream& os, const CircularBuffer& cb) {
+    for (int i = cb.head; i != cb.tail; i = (i + 1) % cb.size) {
+      os << cb.buffer[i] << " ";
+    }
+    return os;
+  }
 };
+
+template <class U>
+U getMax(U a, U b) {
+  return ((a > b) ? a : b);
+}
+
+template<class T>
+CircularBuffer<T> getMax(CircularBuffer<T> a, CircularBuffer<T> b) {
+  return ((a.getSize() > b.getSize()) ? a : b);
+}
+
+
+int main() {
+  CircularBuffer<double> dcb(2);
+  try {
+    dcb.insert(3.3);
+    dcb.insert(4.4);
+    cout << "cb= " << dcb << endl;
+    double r = dcb.remove();
+    cout << "r= " << r << endl;
+    cout << "cb= " << dcb << endl;
+    dcb.insert(5.5);
+    cout << "cb= " << dcb << endl;
+    dcb.insert(6.6);
+  }
+  catch (std::exception& e)
+  {
+    cerr << e.what() << endl;
+  }
+  
+  float f1=1.23, f2=3.21;
+  cout << getMax(f1,f2) << endl;
+  
+  CircularBuffer<int> icb1(1200);
+  CircularBuffer<int> icb2 = icb1;
+  try {
+    icb1.insert(5);
+    cout <<  getMax(icb2,icb1) << endl;
+    icb1.remove();
+    icb1.remove();
+  }
+  catch (std::exception& e)
+  {
+    cerr << e.what() << endl;
+  }
+
+}
+
+
+
+
+
